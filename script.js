09f5ad314f3eb09c13ef9fe9f2000fa42d55c4ce
khaled basta <khaled.basta@protonmail.com>
2022-02-10 23:42:53 +0200

    Generating the hiddenNumber

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 const output = document.querySelector("p");
 const myInput = document.querySelector("input");
 const btn = document.querySelector("button");
-let lowValue = 1;
-let highValue = 10;
-
+let lowValue = 0;
+let highValue = 0;
+let hiddenNumber;
 const getRan = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -18,12 +18,13 @@ const check = function () {
 const initiateGame = function () {
   lowValue = getRan(1, 7);
   highValue = getRan(lowValue + 1, 20);
+  hiddenNumber = getRan(lowValue, highValue);
   output.textContent = `Guess a number between ${lowValue} and ${highValue}`;
   myInput.setAttribute("type", "number");
   myInput.setAttribute("min", lowValue);
   myInput.setAttribute("max", highValue);
   btn.textContent = "Enter Guess";
-  console.log(lowValue, highValue);
+  console.log(lowValue, highValue, hiddenNumber);
 };
 
 initiateGame();
