3af9c587c7e46a2e705fd399a7bf01355dde7f3c
khaled basta <khaled.basta@protonmail.com>
2022-02-10 23:40:47 +0200

    Making iniateGame function

--- script.js
+++ script.js
@@ -3,15 +3,29 @@
 const output = document.querySelector("p");
 const myInput = document.querySelector("input");
 const btn = document.querySelector("button");
-
-const check = function () {
-  let randomNumber = getRan(1, 5);
-  output.textContent += `${randomNumber}, `;
-  console.log(randomNumber);
-};
+let lowValue = 1;
+let highValue = 10;
 
 const getRan = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const check = function () {
+  let randomNumber = getRan(1, 5);
+  console.log(randomNumber);
+};
+
+const initiateGame = function () {
+  lowValue = getRan(1, 7);
+  highValue = getRan(lowValue + 1, 20);
+  output.textContent = `Guess a number between ${lowValue} and ${highValue}`;
+  myInput.setAttribute("type", "number");
+  myInput.setAttribute("min", lowValue);
+  myInput.setAttribute("max", highValue);
+  btn.textContent = "Enter Guess";
+  console.log(lowValue, highValue);
+};
+
+initiateGame();
 
 btn.addEventListener("click", check);
